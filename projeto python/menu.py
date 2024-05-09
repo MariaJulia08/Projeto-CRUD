3bb089401f21136f6e4177bc98ff6178860ea142
Maria Luíza Dantas <164015200+maludantass@users.noreply.github.com>
2024-05-09 08:30:23 -0300

    Add files via upload

---
+++ projeto python/menu.py
@@ -0,0 +1,15 @@
+
+cadastros = {}
+
+
+while True:
+    print('\n''\n''█▀▄▀█ █▀▀ █▄░█ █░█ \n''█░▀░█ ██▄ █░▀█ █▄█''\n''\n''\n''█▀█ █▀█ █ █▄░█ █▀▀ █ █▀█ ▄▀█ █░░''\n''█▀▀ █▀▄ █ █░▀█ █▄▄ █ █▀▀ █▀█ █▄▄''\n''\n'
+          'OPÇÕES:''\N'
+          '[1]: ADICIONAR'
+          '[2]: VISUALIZAR'
+          '[3]: DELETAR'
+          '[4]: ATUALIZAR'
+          '[5]: SALVAR'
+          '[6]: SAIR'
+          )
+    
